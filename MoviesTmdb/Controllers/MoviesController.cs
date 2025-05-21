089ab03dd8c285daa7325d85b6318f3128ed5f73
imaginennn <imaginennn@users.noreply.github.com>
2025-05-22 01:21:51 +0200

    Add controller, service, views

---
+++ MoviesTmdb/Controllers/MoviesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesTmdb.Data;
+using MoviesTmdb.Services;
+
+namespace MoviesTmdb.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MoviesTmdbDbContext _db;
+        private readonly TmdbService _tmdb;
+
+        public MoviesController(MoviesTmdbDbContext db, TmdbService tmdb)
+        {
+            _db = db;
+            _tmdb = tmdb;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = _db.Movies
+                .Include(m => m.MovieGenres).ThenInclude(mg => mg.Genre)
+                .Include(m => m.MovieActors).ThenInclude(ma => ma.Actor)
+                .Include(m => m.Director)
+                .ToList();
+            return View(movies);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> FetchFromTmdb()
+        {
+            var movies = await _tmdb.GetPopularMoviesAsync();
+            _db.Movies.AddRange(movies);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteAll()
+        {
+            _db.Movies.RemoveRange(_db.Movies);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Analysis()
+        {
+            var topDirectors = _db.Directors.OrderByDescending(d => d.Movies.Count).Take(5).ToList();
+            var topActors = _db.Actors.OrderByDescending(a => a.MovieActors.Count).Take(5).ToList();
+
+            var genreByDecade = _db.Movies
+                .Where(m => m.ReleaseDate.HasValue)
+                .GroupBy(m => m.ReleaseDate.Value.Year / 10 * 10)
+                .Select(g => new {
+                    Decade = g.Key,
+                    TopGenre = g.SelectMany(m => m.MovieGenres)
+                                .GroupBy(mg => mg.Genre.Name)
+                                .OrderByDescending(grp => grp.Count())
+                                .Select(grp => grp.Key)
+                                .FirstOrDefault()
+                }).ToList();
+
+            ViewBag.TopDirectors = topDirectors;
+            ViewBag.TopActors = topActors;
+            ViewBag.GenreByDecade = genreByDecade;
+            return View();
+        }
+    }
+}
---
+++ MoviesTmdb/Services/TmdbService.cs
@@ -0,0 +1,36 @@
+﻿using MoviesTmdb.Models;
+using System.Text.Json;
+
+namespace MoviesTmdb.Services
+{
+    public class TmdbService
+    {
+        private readonly HttpClient _client;
+        private const string ApiKey = "53e1dc66f9856493c65dcde5137ad7bb";
+
+        public TmdbService(HttpClient client)
+        {
+            _client = client;
+            _client.BaseAddress = new Uri("https://api.themoviedb.org/3/");
+        }
+
+        public async Task<List<Movie>> GetPopularMoviesAsync()
+        {
+            var response = await _client.GetFromJsonAsync<JsonElement>($"movie/popular?api_key={ApiKey}");
+            var results = response.GetProperty("results");
+
+            var movies = new List<Movie>();
+            foreach (var item in results.EnumerateArray())
+            {
+                movies.Add(new Movie
+                {
+                    TmdbId = item.GetProperty("id").GetInt32(),
+                    Title = item.GetProperty("title").GetString(),
+                    ReleaseDate = item.TryGetProperty("release_date", out var date) &&
+                        DateTime.TryParse(date.GetString(), out var parsedDate) ? parsedDate : null
+                });
+            }
+            return movies;
+        }
+    }
+}
