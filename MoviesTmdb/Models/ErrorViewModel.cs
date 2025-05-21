c7e7a947ccb10198402b9e02c160d7365dff93ba
imaginennn <imaginennn@users.noreply.github.com>
2025-05-22 00:50:27 +0200

    Add gitignore

---
+++ MoviesTmdb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoviesTmdb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
