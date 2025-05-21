eaa11f5c1dc8809bcddec6a9cd66a89dc1d13fa5
imaginennn <imaginennn@users.noreply.github.com>
2025-05-22 01:15:33 +0200

    Add DbConntext and migrations

---
+++ MoviesTmdb/Data/MoviesTmdbDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesTmdb.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace MoviesTmdb.Data
+{
+    public class MoviesTmdbDbContext : DbContext
+    {
+        public MoviesTmdbDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Director> Directors { get; set; }
+        public DbSet<MovieGenre> MovieGenres { get; set; }
+        public DbSet<MovieActor> MovieActors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MovieGenre>().HasKey(mg => new { mg.MovieId, mg.GenreId });
+            modelBuilder.Entity<MovieActor>().HasKey(ma => new { ma.MovieId, ma.ActorId });
+        }
+    }
+}
--- MoviesTmdb/Models/Movie.cs
+++ MoviesTmdb/Models/Movie.cs
@@ -3,12 +3,14 @@
     public class Movie
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public DateTime ReleaseDate { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public DateTime? ReleaseDate { get; set; }
+        public int TmdbId { get; set; }
 
-        public ICollection<Director> Directors { get; set; } = null!;
-        public ICollection<Genre> Genres { get; set; } = null!;
+        public ICollection<MovieGenre> MovieGenres { get; set; } = null!;
         public ICollection<MovieActor> MovieActors { get; set; } = null!;
-    }
 
+        public int? DirectorId { get; set; }
+        public Director Director { get; set; } = null!;
+    }
 }
